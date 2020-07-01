5d16b19790594d81dc059f1653ce8c0bba8274ea
Thomas Sauvajon <thomas@sauvajon.tech>
2020-07-01 18:11:09 +1000

    Create invert-binary-tree.go

---
+++ invert-binary-tree.go
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/invert-binary-tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return root
+    }
+    
+
+    root.Left, root.Right = root.Right, root.Left    
+    
+    
+    invertTree(root.Left)
+    invertTree(root.Right)
+    
+    return root
+}
