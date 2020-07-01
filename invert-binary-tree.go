f136fb09e0745b8ed011b531d3259bb6fe6ef777
Thomas Sauvajon <thomas@sauvajon.tech>
2020-07-01 18:35:36 +1000

    Update invert-binary-tree.go

--- invert-binary-tree.go
+++ invert-binary-tree.go
@@ -12,13 +12,11 @@ func invertTree(root *TreeNode) *TreeNode {
     if root == nil {
         return root
     }
-    
 
     root.Left, root.Right = root.Right, root.Left    
-    
-    
+
     invertTree(root.Left)
     invertTree(root.Right)
-    
+
     return root
 }
